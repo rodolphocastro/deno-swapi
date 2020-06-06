df552fad3de556216323708445072a0e128c73d9
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-06 16:07:46 -0300

    feat films: Read films.json

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
 import { info } from "https://deno.land/std@v0.55.0/log/mod.ts";
+// As of v0.55.0 this module requires the --unstable flag to be used
+import * as fs from "https://deno.land/std@v0.55.0/fs/mod.ts";
+import { Film } from "./models/film.ts";
 
-info("api goes brr brr");
+async function loadDataFromFiles(dataDir: string): Promise<void> {
+    await fs.ensureDir(dataDir);
+    const result = await fs.readJson(dataDir+'/films.json');
+    const films = result as { data: Film[]}
+}
+
+info('Loading data from files')
+await loadDataFromFiles("./data");

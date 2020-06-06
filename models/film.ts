4c5b31c07349d5b8a392bcfb3f0b5a4adc32810c
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-06 16:07:22 -0300

    feat films: Describe FIlm interface

---
+++ models/film.ts
@@ -0,0 +1,16 @@
+export interface Film {
+    title: string;
+    episode_id: number;
+    opening_crawl: string;
+    director: string;
+    producer: string;
+    release_data: string;
+    characters: number[];
+    planets: number[];
+    starships: number[];
+    vehicles: number[];
+    species: number[];
+    created: Date;
+    edited: Date;
+    url: number;
+}

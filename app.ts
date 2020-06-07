accec001f38a7c65ff5ad3f41054edc8683eba18
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-07 10:17:15 -0300

    feat vehicles: Add vehicles endpoints

--- app.ts
+++ app.ts
@@ -4,12 +4,17 @@ import {
   Router,
   Status,
 } from "https://deno.land/x/oak@v5.0.0/mod.ts";
-import { createFilmStateAsync, createSpecieStateAsync } from "./file_utils.ts";
+import {
+  createFilmStateAsync,
+  createSpecieStateAsync,
+  createVehicleStateAsync,
+} from "./file_utils.ts";
 
 info("Loading data from files");
 
 const filmsState = await createFilmStateAsync();
 const speciesState = await createSpecieStateAsync();
+const vehiclesState = await createVehicleStateAsync();
 
 info("Creating the Application");
 
@@ -55,8 +60,29 @@ speciesRouter
     response.status = Status.NotFound;
   });
 
-app.use(filmsRouter.routes());
-app.use(speciesRouter.routes());
+const vehiclesRouter = new Router({ prefix: "/vehicles" });
+vehiclesRouter
+  .get("/", ({ response }) => {
+    response.body = vehiclesState.list();
+    response.status = Status.OK;
+  })
+  .get("/:id", ({ response, params }) => {
+    const { id } = params;
+    const result = vehiclesState.list().filter((f) =>
+      f.url === parseInt(id as string)
+    )[0];
+    if (result) {
+      response.status = Status.OK;
+      response.body = result;
+      return;
+    }
+
+    response.status = Status.NotFound;
+  });
+
+app.use(
+  ...[filmsRouter.routes(), speciesRouter.routes(), vehiclesRouter.routes()],
+);
 
 info("Listening to port 8000");
 await app.listen({ port: 8000 });

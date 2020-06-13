8b6fa80fcbc83c39e9a0a872c2da78b9263d4072
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-13 20:19:40 -0300

    feat planets: Add Planet Interface

---
+++ models/planet.ts
@@ -0,0 +1,16 @@
+export interface Planet {
+  name: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  population: string
+  residents: number[];
+  films: number[];
+  created: Date;
+  edited: Date;
+  url: number;
+}

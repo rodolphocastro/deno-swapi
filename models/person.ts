e6edf6976a2d92c3f1db7e959143fb3382093608
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-06-13 21:04:31 -0300

    feat people: Add Person interface

---
+++ models/person.ts
@@ -0,0 +1,18 @@
+export interface Person {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: number;
+  films: number[];
+  species: number[];
+  vehicles: number[];
+  starships: number[];
+  created: Date;
+  edited: Date;
+  url: number;
+}
